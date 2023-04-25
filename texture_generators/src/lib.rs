aa6890361f285061e9dced2427f9a973a89c31c8
LvanArkel <lvanarkel@hotmail.nl>
2023-04-25 15:50:23 +0200

    Changed texture generators to use f32 instead of u8

--- texture_generators/src/lib.rs
+++ texture_generators/src/lib.rs
@@ -1,18 +1,18 @@
 use std::iter::zip;
 
 use graph::TextureTransformer;
-use image::{ImageBuffer, Rgb, RgbImage, Pixel};
+use image::{ImageBuffer, Rgb, RgbImage, Pixel, DynamicImage, Rgb32FImage};
 
 const WIDTH: u32 = 128;
 const HEIGHT: u32 = 128;
 
 pub struct SolidColor {
-    pub color: Rgb<u8>
+    pub color: Rgb<f32>
 }
 
-impl TextureTransformer<RgbImage> for SolidColor {
-    fn generate(&self, _inputs: Vec<&RgbImage>) -> RgbImage {
-        ImageBuffer::from_fn(WIDTH, HEIGHT, |_,_| -> Rgb<u8> {self.color})
+impl TextureTransformer<Rgb32FImage> for SolidColor {
+    fn generate(&self, _inputs: Vec<&Rgb32FImage>) -> Rgb32FImage {
+        Rgb32FImage::from_pixel(WIDTH, HEIGHT, self.color)
     }
 
     fn inputs(&self) -> usize {
@@ -30,11 +30,11 @@ pub struct Blend {
     pub operator: BlendOptions
 }
 
-impl TextureTransformer<RgbImage> for Blend {
-    fn generate(&self, inputs: Vec<&RgbImage>) -> RgbImage {
+impl TextureTransformer<Rgb32FImage> for Blend {
+    fn generate(&self, inputs: Vec<&Rgb32FImage>) -> Rgb32FImage {
         let i1 = inputs[0];
         let i2 = inputs[1];
-        let mut img: RgbImage = ImageBuffer::new(WIDTH, HEIGHT);
+        let mut img: Rgb32FImage = ImageBuffer::new(WIDTH, HEIGHT);
         for ((x1, y1, pixel1), (_x2, _y2, pixel2)) in zip(i1.enumerate_pixels(), i2.enumerate_pixels()) {
             let pixel = match self.operator {
                 BlendOptions::ADD => pixel1.map2(pixel2, |a, b| a + b),
